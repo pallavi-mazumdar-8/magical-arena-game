b60537405b911c120a7b8f96e0316065aa8399ed
pallavi-mazumdar-8 <pallavimazumdar27@gmail.com>
2024-03-29 20:36:40 +0530

    Initialize the project with the Player and MagicalArena classes.

--- src/arena/magical/Player.java
+++ src/arena/magical/Player.java
@@ -1,77 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
+
 package arena.magical;
 
 import java.util.Random;
 
-/**
- *
- * @author 91708
- */
+
 public class Player {
-    private String name;
-    private int health;
-    private int strength;
-    private int attack;
-
-    public Player(String name, int health, int strength, int attack) {
-        this.name = name;
-        this.health = health;
-        this.strength = strength;
-        this.attack = attack;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getHealth() {
-        return health;
-    }
-
-    public void setHealth(int health) {
-        this.health = health;
-    }
-
-    public int getStrength() {
-        return strength;
-    }
-
-    public int getAttack() {
-        return attack;
-    }
-
-    public void attack(Player opponent) {
-        Random rand = new Random();
-        int attackRoll = rand.nextInt(6) + 1;
-        int defenseRoll = rand.nextInt(6) + 1;
-
-        int damage = attackRoll * this.attack;
-        int defense = defenseRoll * opponent.getStrength();
-
-        System.out.println(this.name + " attacks and rolls die. Die roll: " + attackRoll);
-        System.out.println(opponent.getName() + " defends and rolls die. Die roll: " + defenseRoll);
-        System.out.println("Attack damage is " + attackRoll + " * " + this.attack + " = " + damage);
-        System.out.println("Defending strength is " + defenseRoll + " * " + opponent.getStrength() + " = " + defense);
-        System.out.println();
-       
-
-        
-
-        if (damage > defense) {
-            int excessDamage = damage - defense;
-            opponent.setHealth(opponent.getHealth() - excessDamage);
-            System.out.println(opponent.getName() + " health reduced by " + excessDamage + " to " + opponent.getHealth());
-            System.out.println();
-
-        } else {
-            System.out.println("No damage inflicted on " + opponent.getName() + ".");
-            System.out.println();
-
-        }
-    }
+ 
 
 }
