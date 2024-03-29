94217fe0aeff15c541a1d2b50a9b3af7fe16776c
pallavi-mazumdar-8 <pallavimazumdar27@gmail.com>
2024-03-29 20:53:39 +0530

    Add the main method to take user input in MagicalArena.

--- src/arena/magical/MagicalArena.java
+++ src/arena/magical/MagicalArena.java
@@ -7,7 +7,41 @@ import java.util.Scanner;
 
 public class MagicalArena {
 
-    
+    public static void main(String[] args) {
+     
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter Player A's attributes (health, strength, attack):");
+        int healthA = scanner.nextInt();
+        int strengthA = scanner.nextInt();
+        int attackA = scanner.nextInt();
+        Player playerA = new Player("A", healthA, strengthA, attackA);
+
+        System.out.println("Enter Player B's attributes (health, strength, attack):");
+        int healthB = scanner.nextInt();
+        int strengthB = scanner.nextInt();
+        int attackB = scanner.nextInt();
+        Player playerB = new Player("B", healthB, strengthB, attackB);
+
+        
+        scanner.close();
+
+        while (playerA.getHealth() > 0 && playerB.getHealth() > 0) {
+            playerA.attack(playerB);
+
+            if (playerB.getHealth() <= 0) {
+                System.out.println(playerA.getName() + " wins!");
+                break;
+            }
+
+            playerB.attack(playerA);
+
+            if (playerA.getHealth() <= 0) {
+                System.out.println(playerB.getName() + " wins!");
+                break;
+            }
+        }
+    }
     
     
     
